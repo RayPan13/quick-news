b0f8d56df89c167d877e274f8cc55f2b692d6dac
RayPan <Ray0928066818@gmail.com>
2021-11-11 17:47:44 +0800

    修正 axios base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,7 @@
-const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? { router: { base: '/quick-news/' } } : {}
+const routerBase =
+    process.env.DEPLOY_ENV === 'GH_PAGES'
+        ? { axios: { baseURL: '/quick-news/' }, router: { base: '/quick-news/' } }
+        : {}
 export default {
     // Target: https://go.nuxtjs.dev/config-target
     target: 'static',
@@ -67,12 +70,6 @@ export default {
                 icons: ['fab'],
             },
         ],
-    },
-    axios: {
-        proxy: true,
-    },
-    proxy: {
-        '/api': 'https://raypan13.github.io/quick-news/',
     },
     ...routerBase,
     // Build Configuration: https://go.nuxtjs.dev/config-build
