482c4a197a38f332ac743e15dfa2dc9571c5eb3d
RayPan13 <RayPan13@users.noreply.github.com>
2021-11-11 08:16:49 +0000

    deploy: 8146911eeb050274993ac648d40a8f8f6faac101

--- _nuxt/static/1636618307/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636618307",layout:"default",error:a,state:{isOpen:false},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
