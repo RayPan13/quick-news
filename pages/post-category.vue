6a970ebe6f5041abddc1185afa01261e090cdedd
RayPan <Ray0928066818@gmail.com>
2021-11-16 12:54:14 +0800

    新增 post category 頁面

---
+++ pages/post-category.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="post-category">
+        <main>
+            <the-intro :intro="intro" />
+        </main>
+    </div>
+</template>
+
+<script>
+import TheIntro from '@/components/TheIntro.vue'
+export default {
+    components: { TheIntro },
+    head: {
+        title: 'POST(Category) - Quick News',
+        meta: [
+            { hid: 'title', name: 'title', content: 'POST(Category) - Quick News' },
+            { hid: 'description', name: 'description', content: 'This is POST(Category) - Quick News' },
+        ],
+    },
+    mounted() {
+        this.$store.dispatch('updateNav', false)
+    },
+}
+</script>
