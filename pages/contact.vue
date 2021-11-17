400f4d713f74710e8a7a6be19a6b722ff5f288f3
RayPan <Ray0928066818@gmail.com>
2021-11-17 16:04:54 +0800

    建立 contact 資料

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,6 +2,28 @@
     <div id="contact">
         <main>
             <the-intro :intro="intro" />
+            <div class="container">
+                <div class="info">
+                    <h2>{{ info.title }}</h2>
+                    <p>{{ info.desc }}</p>
+                    <div class="box">
+                        <div v-for="obj of info.contact" :key="obj.id" class="item">
+                            <ul>
+                                <li v-for="item of obj.list" :key="item.id">{{ item.text }}</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="social">
+                        <ul>
+                            <li v-for="obj of info.social" :key="obj.name">
+                                <a :href="obj.url">{{ obj.name }}</a>
+                                <fa :icon="[obj.icon[0], obj.icon[1]]" />
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="form"></div>
+            </div>
             <the-map />
         </main>
     </div>
@@ -19,6 +41,33 @@ export default {
                 subtitle: '',
                 tag: false,
             },
+            info: {
+                title: 'Feel Free to Contact Us',
+                desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sagittis odio sit amet turpis feugiat condimentum nec ac neque. Duis porta, nunc volutpat varius lobortis.',
+                contact: [
+                    {
+                        id: 'left',
+                        list: [
+                            { id: 'addr', icon: '', text: '7457 River Street Buffalo, NY 14224' },
+                            { id: 'email', icon: '', text: 'info@example.com' },
+                            { id: 'phone', icon: '', text: '+123 456 7890' },
+                        ],
+                    },
+                    {
+                        id: 'right',
+                        list: [
+                            { id: 'addr', icon: '', text: '813 Stonybrook Road Champaign, IL 61821' },
+                            { id: 'email', icon: '', text: 'office@example.com' },
+                            { id: 'phone', icon: '', text: '+098 765 4321' },
+                        ],
+                    },
+                ],
+                social: [
+                    { url: 'javascript:;', icon: ['fab', 'facebook-f'], name: 'fb' },
+                    { url: 'javascript:;', icon: ['fab', 'twitter'], name: 'twitter' },
+                    { url: 'javascript:;', icon: ['fab', 'linkedin-in'], name: 'linked' },
+                ],
+            },
         }
     },
     head: {
@@ -33,3 +82,17 @@ export default {
     },
 }
 </script>
+<style lang="scss" scoped>
+@import '@/assets/scss/variable';
+#contact {
+    .container {
+        display: flex;
+    }
+    .info {
+        flex-basis: 50%;
+    }
+    .form {
+        flex-basis: 50%;
+    }
+}
+</style>
