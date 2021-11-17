e4069ce980c4b1957db055bab1fef17ce3dc7d03
RayPan <Ray0928066818@gmail.com>
2021-11-17 14:49:46 +0800

    新增 Contact 頁面

---
+++ pages/contact.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="contact"></div>
+</template>
+<script>
+export default {
+    head: {
+        title: 'Contact - Quick News',
+        meta: [
+            { hid: 'title', name: 'title', content: 'Contact - Quick News' },
+            { hid: 'description', name: 'description', content: 'This is Contact - Quick News' },
+        ],
+    },
+    mounted() {
+        this.$store.dispatch('updateNav', false)
+    },
+}
+</script>
