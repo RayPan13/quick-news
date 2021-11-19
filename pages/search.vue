872cd2586ec6e3ee3b721a8cb4524b35ce8f186d
RayPan <Ray0928066818@gmail.com>
2021-11-19 15:55:33 +0800

    新增 search 頁面

---
+++ pages/search.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="search">
+        <main></main>
+    </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss" scoped>
+@import '@/assets/scss/variable';
+</style>
