5f50b559745f962b5d862efe88d9401472f1b389
RayPan13 <RayPan13@users.noreply.github.com>
2021-11-19 07:14:33 +0000

    deploy: c144c1c33e549ee1d18e631579659eb73eb7f89d

---
+++ _nuxt/static/1637306072/post-category/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637306072",layout:"default",error:a,state:{isOpen:false},serverRendered:true,routePath:"\u002Fpost-category",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
