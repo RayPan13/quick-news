feae3eb31b0ccf60faa71c1859528339c4cdc0c7
RayPan <Ray0928066818@gmail.com>
2021-11-19 16:12:32 +0800

    把 article title 拉出來做獨立元件

--- components/TheArticle.vue
+++ components/TheArticle.vue
@@ -1,16 +1,6 @@
 <template>
     <article>
-        <div class="title">
-            <h2>{{ article.title }}</h2>
-            <div class="social">
-                <ul>
-                    <li v-for="obj of article.socialAry" :key="obj.name">
-                        <a :href="obj.url">{{ obj.name }}</a>
-                        <fa :icon="[obj.icon[0], obj.icon[1]]" />
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <the-article-title :article="article" />
         <div class="box">
             <!-- eslint-disable vue/no-v-html -->
             <section v-for="(obj, index) of article.section" :key="obj.title + index">
@@ -33,10 +23,11 @@
 </template>
 <script>
 import TheAsideCarousel from '@/components/TheAsideCarousel.vue'
+import TheArticleTitle from '@/components/TheArticleTitle.vue'
 
 export default {
     name: 'TheArticle',
-    components: { TheAsideCarousel },
+    components: { TheAsideCarousel, TheArticleTitle },
     props: {
         article: {
             type: Object,
@@ -53,78 +44,7 @@ article {
     display: flex;
     position: relative;
     flex-wrap: wrap;
-    .title {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-bottom: 24px;
-        position: absolute;
-        top: 52px;
-        left: -15%;
-        flex-direction: column;
-        @include media(1200) {
-            position: relative;
-            top: 0;
-            left: 0;
-            flex-basis: 100%;
-            flex-direction: row;
-        }
-        @include media(768) {
-            flex-wrap: wrap;
-        }
-        h2 {
-            font-size: 8rem;
-            color: rgba(0, 0, 0, 0.1);
-            margin: 0;
-            order: 1;
-            transform-origin: center;
-            transform: rotate(-90deg) translate(-80%, 8%);
-            @include media(1200) {
-                font-size: 4rem;
-                transform: rotate(0) translateX(0);
-                order: 0;
-                color: #000;
-            }
-            @include media(768) {
-                flex-basis: 100%;
-                margin-bottom: 24px;
-            }
-        }
-        ul {
-            list-style: none;
-            padding: 0;
-            margin: 0;
-            @include media(1200) {
-                display: flex;
-            }
-            @include media(768) {
-                flex-basis: 100%;
-            }
-        }
-        li {
-            font-size: 2.5rem;
-            position: relative;
-            margin-bottom: 12px;
-            color: #aaa;
-            @include media(1200) {
-                margin-right: 12px;
-                &:last-child {
-                    margin-right: 0;
-                }
-            }
-            &:last-child {
-                margin-bottom: 0;
-            }
-            a {
-                position: absolute;
-                top: 0;
-                left: 0;
-                right: 0;
-                bottom: 0;
-                font-size: 0;
-            }
-        }
-    }
+
     .box {
         flex-basis: calc(100% - 210px);
         padding-right: 5%;
--- pages/search.vue
+++ pages/search.vue
@@ -2,13 +2,17 @@
     <div id="search">
         <main>
             <the-intro :intro="intro" />
+            <div class="container">
+                <the-article-title :article="article" />
+            </div>
         </main>
     </div>
 </template>
 <script>
 import TheIntro from '@/components/TheIntro.vue'
+import TheArticleTitle from '@/components/TheArticleTitle.vue'
 export default {
-    components: { TheIntro },
+    components: { TheIntro, TheArticleTitle },
     data() {
         return {
             intro: {
@@ -17,10 +21,79 @@ export default {
                 subtitle: '',
                 tag: false,
             },
+            article: {
+                title: 'Follow Us',
+                socialAry: [
+                    { url: 'javascript:;', icon: ['fab', 'facebook-f'], name: 'fb' },
+                    { url: 'javascript:;', icon: ['fab', 'twitter'], name: 'twitter' },
+                    { url: 'javascript:;', icon: ['fab', 'linkedin-in'], name: 'linked' },
+                ],
+                section: [
+                    {
+                        title: '',
+                        content: `
+                            <p>Hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line. Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.</p><p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed <a href="javascript:;">base benefits</a>. Dramatically visualize customer directed</p>
+                        `,
+                    },
+                    {
+                        title: 'First Step',
+                        content: `
+                            <p>Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail.</p>
+                            <ul><li>Completely synergize resource taxing relationships</li><li>Professionally cultivate one-to-one customer service with robust ideas</li><li>Dynamically innovate resource-leveling customer service for state</li></ul><h4>Proactively envisioned multimedia based expertise</h4>
+                            <p>Completely synergize resource taxing relationships via premier niche markets. Professionally cultivate one-to-one customer service with robust ideas.</p>
+                        `,
+                    },
+                    {
+                        title: 'Second Step',
+                        content: `
+                            <ol><li>Completely synergize resource taxing relationships</li><li>Professionally cultivate one-to-one customer service with robust ideas</li><li>Dynamically innovate resource-leveling customer service for state</li></ol><p>Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail.</p>
+                        `,
+                    },
+                    {
+                        title: '',
+                        content: `
+                            <p class="end">Thanks for reading this!</p>
+                        `,
+                    },
+                ],
+                hot: [
+                    {
+                        id: 1001,
+                        url: 'javascript:;',
+                        title: '19 Top Tourists Attractions in United States this summer!',
+                        date: '2021/11/12',
+                    },
+                    {
+                        id: 1002,
+                        url: 'javascript:;',
+                        title: '19 Top Tourists Attractions!',
+                        date: '2021/11/12',
+                    },
+                ],
+                related: [
+                    {
+                        id: 2001,
+                        url: 'javascript:;',
+                        title: '19 Top Tourists Attractions in United States this summer!',
+                        date: '2021/11/12',
+                    },
+                    {
+                        id: 2002,
+                        url: 'javascript:;',
+                        title: '19 Top Tourists Attractions!',
+                        date: '2021/11/12',
+                    },
+                ],
+            },
         }
     },
 }
 </script>
 <style lang="scss" scoped>
 @import '@/assets/scss/variable';
+.search {
+    .container {
+        position: relative;
+    }
+}
 </style>
