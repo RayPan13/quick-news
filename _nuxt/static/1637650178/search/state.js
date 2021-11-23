bb514d7d3432d6a4678abe5d26b399caf931d417
RayPan13 <RayPan13@users.noreply.github.com>
2021-11-23 07:27:16 +0000

    deploy: 6c9a17b147a8144d0e8ec3e6fb562a75330dc3fb

--- _nuxt/static/1637650178/search/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fquick-news\u002F_nuxt\u002Fstatic\u002F1637650178",layout:"default",error:a,state:{isOpen:false},serverRendered:true,routePath:"\u002Fsearch",config:{axios:{baseURL:b},_app:{basePath:b,assetsPath:"\u002Fquick-news\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002Fquick-news\u002F"));
